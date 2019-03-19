02ae8d018f86465ccd57da7fa291c9641a9f96ee
enzo <36193661+enzofragan@users.noreply.github.com>
2019-03-19 09:43:01 -0300

    primer proyecto de c#

---
+++ Fragan.Enzo/proyecto01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyecto01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("hola mundo!!!!!!");
+
+            System.Console.ReadKey();
+        }
+    }
+}
