9f9937e1e0b52083e82ed235e3b2e4a8dfc4e0e5
alumno <alumno@utnfra.com.ar>
2019-05-14 12:03:20 -0300

    clase 14

--- Fragan.Enzo/clase 14/parseadoraDeEnteros.cs
+++ Fragan.Enzo/clase 14/parseadoraDeEnteros.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +6,38 @@ using System.Threading.Tasks;
 
 namespace clase_14
 {
-  class parseadoraDeEnteros
+  public static class parseadoraDeEnteros
   {
+    public static int Parse(string cadena)
+    {
+      int numero;
+
+      try
+      {
+        numero = int.Parse(cadena);
+      }
+      catch (FormatException e)
+      {
+        throw new Exception ("El string no podrá ser convertido por error de formato",e);
+      }
+      catch (OverflowException f)
+      {  
+        throw new Exception ("El string no podrá ser convertido por tamaño del dato",f);
+      }
+
+      return numero;
+    }
+
+    public static bool TryParse(string cadena, out int entero)
+    {
+      bool respuesta = false;
+      entero = 0;
+      if(Parse(cadena) != 0)
+      {
+        respuesta = true;
+      }
+
+      return respuesta;
+    }
   }
 }
